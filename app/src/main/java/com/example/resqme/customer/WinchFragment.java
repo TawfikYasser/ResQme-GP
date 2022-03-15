3d252bd4df03598119c9451ac7f936d7cc7d4b5d
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2022-03-15 19:25:56 +0200

    Winch Map UI Added.

--- app/src/main/java/com/example/resqme/customer/WinchFragment.java
+++ app/src/main/java/com/example/resqme/customer/WinchFragment.java
@@ -2,6 +2,7 @@ package com.example.resqme.customer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +10,61 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.resqme.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WinchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class WinchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class WinchFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public WinchFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WinchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WinchFragment newInstance(String param1, String param2) {
-        WinchFragment fragment = new WinchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
+    }
+    private SupportMapFragment mapFragment;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_winch, container, false);
+        View view =  inflater.inflate(R.layout.fragment_winch, container, false);
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+//            mapFragment.getMapAsync(new OnMapReadyCallback() {
+//                @Override
+//                public void onMapReady(GoogleMap googleMap) {
+//                    LatLng latLng = new LatLng(1.289545, 103.849972);
+//                    googleMap.addMarker(new MarkerOptions().position(latLng)
+//                            .title("Singapore"));
+//                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+//
+//                    googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//                        @Override
+//                        public void onMapClick(@NonNull LatLng latLng) {
+//
+//                        }
+//                    });
+//
+//                }
+//            });
+        }
+        getChildFragmentManager().beginTransaction().replace(R.id.fragment_map_winchs, mapFragment).commit();
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
