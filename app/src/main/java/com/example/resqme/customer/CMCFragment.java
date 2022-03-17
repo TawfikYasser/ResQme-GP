1b7bf0ec66ad9782ce99aaed4c5b1c9b79013891
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2022-03-17 18:07:09 +0200

    Fixes in Customer Home, Fixes in Add Service for Service Provider.

--- app/src/main/java/com/example/resqme/customer/CMCFragment.java
+++ app/src/main/java/com/example/resqme/customer/CMCFragment.java
@@ -49,19 +49,19 @@ public class CMCFragment extends Fragment {
 
         cmcRV = view.findViewById(R.id.cmc_recycler);
         context = getActivity().getApplicationContext();
-        //sparepartsDB = FirebaseDatabase.getInstance().getReference().child("SpareParts");
+        //cmcDB = FirebaseDatabase.getInstance().getReference().child("CMCs");
         cmcRV.setHasFixedSize(true);
         cmcRV.setLayoutManager(new LinearLayoutManager(getActivity()));
         cmcs = new ArrayList<>();
 
-        cmcs.add(new CMC("","مركز البطل أوتو بلس", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/UserImages%2FIMG_20220316_134648913.jpg?alt=media&token=f86bd598-5476-4562-8dcf-e15f36643ee5"
+        cmcs.add(new CMC("","مركز البطل أوتو بلس", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/cmcimage.jpg?alt=media&token=a7b4cf99-4d57-414d-ac62-a466f73dd000"
                 ,"الطريق الدائرى . نزلة الكهرباء بجوار، محور 26 يوليو", "BMW", "", ""));
 
-        cmcs.add(new CMC("","مركز الفاروق لخدمة السيارات", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/UserImages%2FIMG_20220316_134648913.jpg?alt=media&token=f86bd598-5476-4562-8dcf-e15f36643ee5"
+        cmcs.add(new CMC("","مركز الفاروق لخدمة السيارات", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/cmcimage.jpg?alt=media&token=a7b4cf99-4d57-414d-ac62-a466f73dd000"
                 ,"الطريق الدائري، أثر النبي، حي مصر القديمة", "الجميع", "", ""));
 
 
-        cmcs.add(new CMC("","Renault Moqatam Service Center", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/UserImages%2FIMG_20220316_134648913.jpg?alt=media&token=f86bd598-5476-4562-8dcf-e15f36643ee5"
+        cmcs.add(new CMC("","Renault Moqatam Service Center", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/cmcimage.jpg?alt=media&token=a7b4cf99-4d57-414d-ac62-a466f73dd000"
                 ,"Al Abageyah, El Khalifa, Cairo Governorate", "Renault", "", ""));
 
 
--- app/src/main/java/com/example/resqme/customer/SpareFragment.java
+++ app/src/main/java/com/example/resqme/customer/SpareFragment.java
@@ -53,13 +53,13 @@ public class SpareFragment extends Fragment {
         sparepartsRV.setLayoutManager(new LinearLayoutManager(getActivity()));
         spareParts = new ArrayList<>();
 
-        spareParts.add(new SparePart("","إطارات West Lake ", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/UserImages%2FIMG_20220316_134648913.jpg?alt=media&token=f86bd598-5476-4562-8dcf-e15f36643ee5"
+        spareParts.add(new SparePart("","إطارات West Lake ", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/sparepartsdefaultimage.jpg?alt=media&token=2a65e70b-716c-4cff-b00d-31b5118b0cda"
         ,"500 جنيه", "جديد", "", "","مرسيدس"));
 
-        spareParts.add(new SparePart("","طقم مرايا يمين وشمال", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/UserImages%2FIMG_20220316_134648913.jpg?alt=media&token=f86bd598-5476-4562-8dcf-e15f36643ee5"
+        spareParts.add(new SparePart("","طقم مرايا يمين وشمال", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/sparepartsdefaultimage.jpg?alt=media&token=2a65e70b-716c-4cff-b00d-31b5118b0cda"
                 ,"300 جنيه", "مستعمل", "", "","هونداي"));
 
-        spareParts.add(new SparePart("","مضخم صوت", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/UserImages%2FIMG_20220316_134648913.jpg?alt=media&token=f86bd598-5476-4562-8dcf-e15f36643ee5"
+        spareParts.add(new SparePart("","مضخم صوت", "https://firebasestorage.googleapis.com/v0/b/resqme-60664.appspot.com/o/sparepartsdefaultimage.jpg?alt=media&token=2a65e70b-716c-4cff-b00d-31b5118b0cda"
                 ,"270 جنيه", "جديد", "", "","الجميع"));
 
         sparepartsAdapter = new SparePartsAdapter(getActivity(), spareParts);
