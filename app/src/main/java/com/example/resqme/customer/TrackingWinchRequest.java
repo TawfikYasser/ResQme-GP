bc4e53a1c8d0253f53cc60709a35a7d702a4585f
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2022-03-27 16:17:27 +0200

    Winch Request Design Changed.

---
+++ app/src/main/java/com/example/resqme/customer/TrackingWinchRequest.java
@@ -0,0 +1,16 @@
+package com.example.resqme.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.resqme.R;
+
+public class TrackingWinchRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking_winch_request);
+    }
+}
