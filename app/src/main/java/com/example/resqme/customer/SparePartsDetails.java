e59fd01fefdf735260145cc391b9f0cb284a6727
mariam <mariam.ehab31@outlook.com>
2022-03-27 22:14:09 +0200

    spare parts details ui

---
+++ app/src/main/java/com/example/resqme/customer/SparePartsDetails.java
@@ -0,0 +1,16 @@
+package com.example.resqme.customer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.resqme.R;
+
+public class SparePartsDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spareparts_details);
+    }
+}
