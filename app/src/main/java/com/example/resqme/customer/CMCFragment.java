0126e4a4907f09d2456d70c0462ee79615b875b5
Tawfik <tawfekyassertawfek@gmail.com>
2022-05-16 01:58:22 +0200

    Shimmer Added To CMC

--- app/src/main/java/com/example/resqme/customer/CMCFragment.java
+++ app/src/main/java/com/example/resqme/customer/CMCFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import com.example.resqme.common.MyReportAdapter;
 import com.example.resqme.model.CMC;
 import com.example.resqme.model.Report;
 import com.example.resqme.model.SparePart;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -49,12 +51,15 @@ public class CMCFragment extends Fragment {
     ArrayList<CMC> cmcs;
     Context context;
 
+    ShimmerFrameLayout shimmerFrameLayoutCMCCustomer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_c_m_c, container, false);
-
+        shimmerFrameLayoutCMCCustomer = view.findViewById(R.id.cmc_customer_shimmer);
+        shimmerFrameLayoutCMCCustomer.startShimmer();
         cmcRV = view.findViewById(R.id.cmc_recycler);
         context = getActivity().getApplicationContext();
         cmcDB = FirebaseDatabase.getInstance().getReference().child("CMCs");
@@ -63,6 +68,17 @@ public class CMCFragment extends Fragment {
         cmcs = new ArrayList<>();
         cmcAdapter = new CMCAdapter(getActivity(), cmcs);
         cmcRV.setAdapter(cmcAdapter);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                shimmerFrameLayoutCMCCustomer.stopShimmer();
+                shimmerFrameLayoutCMCCustomer.setVisibility(View.GONE);
+                cmcRV.setVisibility(View.VISIBLE);
+            }  //end of run
+        }, 2000);
 
 
         cmcDB.addValueEventListener(new ValueEventListener() {
