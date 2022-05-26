de7131fa4e4754f59d8663886fa8f0a47516b433
Tawfik <tawfekyassertawfek@gmail.com>
2022-05-26 23:24:59 +0200

    Log Work

--- app/src/main/java/com/example/resqme/customer/CustomerHome.java
+++ app/src/main/java/com/example/resqme/customer/CustomerHome.java
@@ -2,14 +2,18 @@ package com.example.resqme.customer;
 
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,27 +26,48 @@ import com.bumptech.glide.Glide;
 import com.example.resqme.R;
 import com.example.resqme.common.InternetConnection;
 import com.example.resqme.common.LogData;
+import com.example.resqme.model.LogDataModel;
+import com.example.resqme.model.WinchRequest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CustomerHome extends AppCompatActivity implements View.OnClickListener{
-    CircleImageView customerProfile;
+    CircleImageView customerProfile, infoImage;
     TextView headerTV;
     InternetConnection ic;
     MaterialButton orders, cart;
+    DatabaseReference logDB;
+    ArrayList<LogDataModel> logs;
+    ArrayList<String> times;
+    int avgTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
         initViews();
         forceRTLIfSupported();
+        logDB = FirebaseDatabase.getInstance().getReference().child("LOG");
+        logs = new ArrayList<>();
+        times = new ArrayList<>();
+        infoImage = findViewById(R.id.info_customer_home);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         if (savedInstanceState == null) {
@@ -78,8 +103,101 @@ public class CustomerHome extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+
+        logDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    LogDataModel logData = dataSnapshot.getValue(LogDataModel.class);
+                    if(logData.getEventType().equals("SERVICE_CLICK") && logData.getServiceName().equals("WINCH")){
+                        logs.add(logData);
+                    }
+                }
+
+                DatabaseReference winchRequests = FirebaseDatabase.getInstance().getReference().child("WinchRequests");
+                winchRequests.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            WinchRequest winchRequest = dataSnapshot.getValue(WinchRequest.class);
+                            // itervate over logs and check if the winch request is in the logs
+                            for (LogDataModel logData : logs) {
+                                if(logData.getServiceID().equals(winchRequest.getWinchID())){
+                                    // Calculate the time difference between the request and the log
+                                    // convert string to date
+                                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                    Date logDate = null, requestDate = null;
+                                    try {
+                                        logDate = sdf.parse(logData.getLogTimestamp());
+                                        requestDate = sdf.parse(winchRequest.getWinchRequestInitiationDate());
+                                    }
+                                    catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    // Get the difference in milliseconds
+                                    long diff = requestDate.getTime()- logDate.getTime();
+                                    // Convert to minutes
+                                    long diffMinutes = diff / (60 * 1000) % 60;
+                                    times.add(String.valueOf(diffMinutes));
+                                }
+                            }
+                        }
+                        if(times.size() > 0){
+                            // Calculate the average time
+                            int sum = 0;
+                            for (String time : times) {
+                                sum += Integer.parseInt(time);
+
+                            }
+                            int average = sum / times.size();
+                            methodToProcess(average);
+                            infoImage.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        infoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Dialog dialog = new Dialog(CustomerHome.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.info_dialog);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                dialog.setCancelable(true);
+                dialog.findViewById(R.id.info_dialog_text);
+                dialog.show();
+                TextView textInDialog = (TextView) dialog.findViewById(R.id.info_dialog_text);
+
+                if(avgTime == 2){
+                    textInDialog.setText("هل تعلم أن عملية طلب الونش لا تزيد عن دقيقتين؟ أطلب الآن في أسرع وقت");
+                }else if(avgTime == 3 || avgTime == 4|| avgTime == 5 || avgTime == 6 || avgTime == 7 || avgTime == 8 || avgTime == 9 || avgTime == 10){
+                    textInDialog.setText("هل تعلم أن عملية طلب الونش لا تزيد عن "+avgTime+" دقائق؟ أطلب الآن في أسرع وقت");
+                }else{
+                    textInDialog.setText("هل تعلم أن عملية طلب الونش لا تزيد عن "+avgTime+" دقيقة؟ أطلب الآن في أسرع وقت");
+                }
+            }
+        });
+
     }
 
+    private void methodToProcess(int average) {
+        avgTime = average;
+    }
 
     void initViews(){
         customerProfile = findViewById(R.id.customer_home_image);
--- app/src/main/java/com/example/resqme/customer/WinchFragment.java
+++ app/src/main/java/com/example/resqme/customer/WinchFragment.java
@@ -517,7 +517,7 @@ public class WinchFragment extends Fragment implements View.OnClickListener {
                 winchSheetView.findViewById(R.id.btnSheet).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        LogData.saveLog("APP_CLICK","","","CLICK ON ORDER WINCH NOW BUTTON", "WINCH");
+                        LogData.saveLog("SERVICE_CLICK",finalBestWinch.getWinchID(),"WINCH","", "WINCH");
                         // Going to processing winch request page to get the description
                         Intent wpr = new Intent(getActivity(), ProcessingRequestWinch.class);
                         wpr.putExtra("PAYMENT_COST",serviceCost);
